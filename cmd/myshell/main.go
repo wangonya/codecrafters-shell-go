da186be55a9e11cd71c4dcf8e76c456d34d58e7b
wangonya <kwangonya@gmail.com>
2025-02-21 10:46:40 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,21 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
+
+var validCommands = []string{"echo", "exit", "type"}
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		response, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		if response == "exit 0\n" {
+		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		command = strings.TrimRight(command, "\n")
+		if command == "exit 0" {
 			os.Exit(0)
-		} else if strings.HasPrefix(response, "echo") {
-			fmt.Print(response[5:])
+		} else if strings.HasPrefix(command, "echo ") {
+			fmt.Println(command[5:])
+		} else if strings.HasPrefix(command, "type ") {
+			if slices.Contains(validCommands, command[5:]) {
+				fmt.Printf("%s is a shell builtin\n", command[5:])
+			} else {
+				fmt.Printf("%s: not found\n", command[5:])
+			}
 		} else {
-			fmt.Printf("%s: command not found\n", strings.TrimRight(response, "\n"))
+			fmt.Printf("%s: command not found\n", command)
 		}
 	}
 }
