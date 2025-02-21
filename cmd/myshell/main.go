ff1a8798405fe96dab098884cc2658efd5faf24f
wangonya <kwangonya@gmail.com>
2025-02-21 20:44:48 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,7 +8,20 @@ import (
 	"strings"
 )
 
-var validCommands = []string{"echo", "exit", "type"}
+var builtins = []string{"echo", "exit", "type"}
+
+// commandExistsInPath checks if the command exists in any of the locations in $PATH
+//
+// If a match is found, the path location is returned, otherwise err
+func commandExistsInPath(command string) (string, error) {
+	for _, path := range strings.Split(os.Getenv("PATH"), ":") {
+		if _, err := os.Stat(fmt.Sprintf("%s/%s", path, command)); err != nil {
+			continue
+		}
+		return path, nil
+	}
+	return "", fmt.Errorf("%s: not found", command)
+}
 
 func main() {
 	for {
@@ -23,10 +36,15 @@ func main() {
 		} else if strings.HasPrefix(command, "echo ") {
 			fmt.Println(command[5:])
 		} else if strings.HasPrefix(command, "type ") {
-			if slices.Contains(validCommands, command[5:]) {
-				fmt.Printf("%s is a shell builtin\n", command[5:])
+			if slices.Contains(builtins, command[5:]) {
+				fmt.Println(command[5:], "is a shell builtin")
+				continue
+			}
+			path, err := commandExistsInPath(command[5:])
+			if err != nil {
+				fmt.Printf("%s\n", err.Error())
 			} else {
-				fmt.Printf("%s: not found\n", command[5:])
+				fmt.Printf("%s is %s\n", command[5:], fmt.Sprintf("%s/%s", path, command[5:]))
 			}
 		} else {
 			fmt.Printf("%s: command not found\n", command)
