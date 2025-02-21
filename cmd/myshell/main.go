81c6fc0cade6e034da303fbc731b6ebdf8c08736
wangonya <kwangonya@gmail.com>
2025-02-21 06:26:58 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -15,7 +15,10 @@ func main() {
 		response, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		if response == "exit 0\n" {
 			os.Exit(0)
+		} else if strings.HasPrefix(response, "echo") {
+			fmt.Print(response[5:])
+		} else {
+			fmt.Printf("%s: command not found\n", strings.TrimRight(response, "\n"))
 		}
-		fmt.Printf("%s: command not found\n", strings.TrimRight(response, "\n"))
 	}
 }
