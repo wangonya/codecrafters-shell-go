1d1c969756eb24d7823df9fd2cb8c2f3b2298add
wangonya <kwangonya@gmail.com>
2025-02-21 06:12:10 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	response, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Printf("%s: command not found", strings.TrimRight(response, "\n"))
 }
