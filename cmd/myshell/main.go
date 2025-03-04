a1857e9500cbc5f584fa202c0254ed1eae3f8437
wangonya <kwangonya@gmail.com>
2025-03-04 06:32:55 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -37,8 +37,40 @@ func runCmd(cmd command) (string, error) {
 		return "", err
 	}
 
-	out, err := exec.Command(cmd.executable, cmd.args...).Output()
+	// out, err := exec.Command(cmd.executable, cmd.args...).Output()
+	x := []string{cmd.executable}
+	x = append(x, cmd.args...)
+	y := strings.Join(x, " ")
+	// fmt.Println("y ->", y)
+	out, err := exec.Command("sh", "-c", y).CombinedOutput()
+	// fmt.Println("x ->", string(out), err)
 	return string(out), err
+}
+
+// filterEmptyArgs removes any leading and trailing spaces that
+// may be inteprated as args
+func filterEmptyArgs(args []string) []string {
+	x := []string{}
+	inSingleQuotes := false
+	for _, v := range args {
+		// fmt.Println("v1 ->", v)
+		// fmt.Println("strings.Contains(v, ')", strings.Contains(v, "'"))
+		if strings.Contains(v, "'") {
+			inSingleQuotes = !inSingleQuotes
+			// continue
+		}
+
+		// if inSingleQuotes {
+		// 	continue
+		// }
+
+		// fmt.Println("v, insingle ->", v, inSingleQuotes)
+
+		if v != "" || inSingleQuotes {
+			x = append(x, v)
+		}
+	}
+	return x
 }
 
 func main() {
@@ -53,11 +85,17 @@ func main() {
 		}
 
 		splitInput := strings.Split(strings.TrimRight(input, "\n"), " ")
+		// fmt.Println("split 1 ->", splitInput)
+		splitInput = filterEmptyArgs(splitInput)
+		// fmt.Println("split 1 ->", splitInput)
 		cmd := command{splitInput[0], splitInput[1:]}
 
 		switch cmd.executable {
 		case "echo":
 			out, _ := runCmd(cmd)
+			// fmt.Println(cmd)
+			// fmt.Println(out)
+			out = strings.Replace(out, "'", "", -1)
 			fmt.Print(out)
 		case "exit":
 			exitCode, err := strconv.Atoi(cmd.args[0])
@@ -88,6 +126,12 @@ func main() {
 			if err != nil {
 				fmt.Printf("cd: %s: No such file or directory\n", path)
 			}
+		case "cat":
+			out, err := runCmd(cmd)
+			if err != nil {
+				fmt.Println("err ->", err)
+			}
+			fmt.Print(out)
 		default:
 			out, err := runCmd(cmd)
 			if err != nil {
